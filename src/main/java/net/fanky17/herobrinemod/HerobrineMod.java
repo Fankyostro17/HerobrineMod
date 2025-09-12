c3c5ee3354a210e3e3d677969888ed3de980cd6f
thomas fanciullacci <thomas.fanciullacci@gmail.com>
2025-09-12 14:40:19 +0200

    Workspace setup

---
+++ src/main/java/net/fanky17/herobrinemod/HerobrineMod.java
@@ -0,0 +1,17 @@
+package net.fanky17.herobrinemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HerobrineMod implements ModInitializer {
+
+	public static final String MOD_ID = "herobrinemod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/fanky17/herobrinemod/HerobrineModClient.java
@@ -0,0 +1,11 @@
+package net.fanky17.herobrinemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HerobrineModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
